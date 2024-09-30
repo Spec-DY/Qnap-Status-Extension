9a01ed16058659a044a7ba6e2f0fa566b3c7c1be
Spec-DY <ibonehp@gmail.com>
2024-09-29 20:47:37 -0700

    webUI with cpu&ram display

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
 		<header className="App-header">
-			<h1>Display something</h1>
 			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Login />} />
